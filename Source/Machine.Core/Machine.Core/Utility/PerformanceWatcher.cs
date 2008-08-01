04991990b25d551f17964b704b75d36013c48d07
Jacob Lewallen <jlewallen@gmail.com>
2008-08-01 01:00:59 +0000

    Added PerformanceWatcher/IDisposable thing.

---
+++ Source/Machine.Core/Machine.Core/Utility/PerformanceWatcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using log4net;
+
+namespace Machine.Core.Utility
+{
+  public class PerformanceWatcher : IDisposable
+  {
+    private static readonly ILog _primaryLog = LogManager.GetLogger(typeof(PerformanceWatcher));
+    private readonly string _name;
+    private readonly Stopwatch _stopwatch;
+    private readonly ILog _log;
+
+    public PerformanceWatcher(string name)
+     : this(_primaryLog, name)
+    {
+    }
+
+    public PerformanceWatcher(ILog log, string name)
+    {
+      _log = log;
+      _name = name;
+      _stopwatch = new Stopwatch();
+      _stopwatch.Start();
+    }
+
+    public void Dispose()
+    {
+      _stopwatch.Stop();
+      _log.Info(MakeMessage());
+    }
+
+    private string MakeMessage()
+    {
+      return String.Format("{0}: {1}", _name, _stopwatch.Elapsed.TotalSeconds);
+    }
+  }
+}
