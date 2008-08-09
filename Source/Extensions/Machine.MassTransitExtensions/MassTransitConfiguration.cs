aa512c8d28a149a4602f853791a0cf8565c1df9e
Jacob Lewallen <jlewallen@gmail.com>
2008-08-09 05:26:51 +0000

    Removing obsolete stuff in MassTransitConfiguration.
    No longer need DefaultLocalEndpointUri or IsSubscriptionManager, the
    application can create a SubscriptionManagerHub if they need one.

--- Source/Extensions/Machine.MassTransitExtensions/MassTransitConfiguration.cs
+++ Source/Extensions/Machine.MassTransitExtensions/MassTransitConfiguration.cs
@@ -4,27 +4,13 @@ namespace Machine.MassTransitExtensions
 {
   public class MassTransitConfiguration
   {
-    private bool _isSubscriptionManager;
     private Type _transportType;
-    private Uri _defaultLocalEndpointUri;
     private Uri _subscriptionManagerEndpointUri;
-
-    public bool IsSubscriptionManager
-    {
-      get { return _isSubscriptionManager; }
-      set { _isSubscriptionManager = value; }
-    }
 
     public Type TransportType
     {
       get { return _transportType; }
       set { _transportType = value; }
-    }
-
-    public Uri DefaultLocalEndpointUri
-    {
-      get { return _defaultLocalEndpointUri; }
-      set { _defaultLocalEndpointUri = value; }
     }
 
     public Uri SubscriptionManagerEndpointUri
@@ -33,20 +19,13 @@ namespace Machine.MassTransitExtensions
       set { _subscriptionManagerEndpointUri = value; }
     }
 
-    public MassTransitConfiguration(string subscriptionEndpointUri, string localEndpointUri)
-     : this(new Uri(subscriptionEndpointUri), new Uri(localEndpointUri), false)
+    public MassTransitConfiguration(string subscriptionEndpointUri)
+     : this(new Uri(subscriptionEndpointUri))
     {
     }
 
-    public MassTransitConfiguration(string subscriptionEndpointUri, string localEndpointUri, bool isSubscriptionManager)
-     : this(new Uri(subscriptionEndpointUri), new Uri(localEndpointUri), isSubscriptionManager)
+    public MassTransitConfiguration(Uri subscriptionEndpointUri)
     {
-    }
-
-    public MassTransitConfiguration(Uri subscriptionEndpointUri, Uri localEndpointUri, bool isSubscriptionManager)
-    {
-      _isSubscriptionManager = isSubscriptionManager;
-      _defaultLocalEndpointUri = localEndpointUri;
       _subscriptionManagerEndpointUri = subscriptionEndpointUri;
       _transportType = typeof(MassTransit.ServiceBus.MSMQ.MsmqEndpoint);
     }
