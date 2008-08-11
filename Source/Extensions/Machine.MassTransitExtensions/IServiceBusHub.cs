04203203c6262beef33fdf7b6b21178574dd8b03
Jacob Lewallen <jlewallen@gmail.com>
2008-08-11 06:33:51 +0000

    Removing unnecessary members on IServiceBusHub.

--- Source/Extensions/Machine.MassTransitExtensions/IServiceBusHub.cs
+++ Source/Extensions/Machine.MassTransitExtensions/IServiceBusHub.cs
@@ -1,21 +1,10 @@
 using MassTransit.ServiceBus;
-using MassTransit.ServiceBus.Subscriptions;
 
 namespace Machine.MassTransitExtensions
 {
   public interface IServiceBusHub : IHostedService
   {
     IServiceBus Bus
-    {
-      get;
-    }
-
-    ISubscriptionCache SubscriptionCache
-    {
-      get;
-    }
-
-    ISubscriptionService SubscriptionService
     {
       get;
     }
