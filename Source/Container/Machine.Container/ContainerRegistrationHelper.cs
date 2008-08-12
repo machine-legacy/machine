b10910205ea6fa1a51e001d64be28a22b7e6ee2f
Jacob Lewallen <jlewallen@gmail.com>
2008-08-12 05:35:32 +0000

    AddServiceCollection code in ContainerRegistrationHelper.
    Code to iterate over an assembly and add all of the service collections
    inside. That means adding them to the container and resolving them so
    that they can have dependencies.
    
    The only real issue here is if we need to specify some kind of order. Not
    sure how I would like to do that just yet. I suppose people can just
    inject them and decide themselves.

--- Source/Container/Machine.Container/ContainerRegistrationHelper.cs
+++ Source/Container/Machine.Container/ContainerRegistrationHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Reflection;
 
 using Machine.Container.Model;
+using Machine.Container.Plugins;
 using Machine.Container.Services;
 using Machine.Core.Services.Impl;
 using Machine.Core.Utility;
@@ -47,6 +48,34 @@ namespace Machine.Container
       _container.Register.Type<FileSystem>();
       _container.Register.Type<DotNetObjectActivator>();
       _container.Register.Type<ThreadManager>();
+    }
+
+    public virtual void AddServiceCollectionsFrom(Assembly assembly)
+    {
+      foreach (Type type in assembly.GetTypes())
+      {
+        if (typeof(IServiceCollection).IsAssignableFrom(type))
+        {
+          AddServiceCollection(type);
+        }
+      }
+    }
+
+    public virtual void AddServiceCollection<T>() where T : IServiceCollection
+    {
+      AddServiceCollection(typeof(T));
+    }
+
+    public virtual void AddServiceCollection(Type serviceCollectionType)
+    {
+      if (!typeof(IServiceCollection).IsAssignableFrom(serviceCollectionType)) throw new ArgumentException("serviceCollectionType");
+      _container.Register.Type(serviceCollectionType);
+      AddServiceCollection((IServiceCollection)_container.Resolve.Object(serviceCollectionType));
+    }
+
+    public virtual void AddServiceCollection(IServiceCollection services)
+    {
+      services.RegisterServices(_container.Register);
     }
   }
 }
