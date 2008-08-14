0bbdba68157bf84b75da5aa2b08ae9033173dda0
Jacob Lewallen <jlewallen@gmail.com>
2008-08-14 00:01:38 +0000

    Converted MassTransitPlugin into an IServiceCollection.
    All it had was a bunch of registrations, so this is preferrable.

--- Source/Extensions/Machine.MassTransitExtensions/MassTransitPlugin.cs
+++ Source/Extensions/Machine.MassTransitExtensions/MassTransitPlugin.cs
@@ -1,44 +1,29 @@
 using System;
 using System.Collections.Generic;
 
+using Machine.Container;
 using Machine.Container.Plugins;
-using Machine.Container.Services;
 
 using MassTransit.ServiceBus.Internal;
 using MassTransit.ServiceBus.Subscriptions;
 
 namespace Machine.MassTransitExtensions
 {
-  public class MassTransitPlugin : IServiceContainerPlugin
+  public class MassTransitExtensionServices : IServiceCollection
   {
-    #region IServiceContainerPlugin Members
-    public virtual void Initialize(PluginServices services)
+    #region IServiceCollection Members
+    public void RegisterServices(ContainerRegisterer register)
     {
-    }
-
-    public virtual void ReadyForServices(PluginServices services)
-    {
-      RegisterServices(services.Container);
-    }
-    #endregion
-
-    #region IDisposable Members
-    public void Dispose()
-    {
+      register.Type<MassTransitController>();
+      register.Type<MassTransitUriFactory>();
+      register.Type<FollowerRepository>();
+      register.Type<EndpointResolver>();
+      register.Type<LocalSubscriptionCache>();
+      register.Type<HostedServicesController>();
+      register.Type<MachineObjectBuilder>();
+      register.Type<ServiceBusFactory>();
+      register.Type<ServiceBusHubFactory>();
     }
     #endregion
-
-    public virtual void RegisterServices(IMachineContainer container)
-    {
-      container.Register.Type<MassTransitController>();
-      container.Register.Type<MassTransitUriFactory>();
-      container.Register.Type<FollowerRepository>();
-      container.Register.Type<EndpointResolver>();
-      container.Register.Type<LocalSubscriptionCache>();
-      container.Register.Type<HostedServicesController>();
-      container.Register.Type<MachineObjectBuilder>();
-      container.Register.Type<ServiceBusFactory>();
-      container.Register.Type<ServiceBusHubFactory>();
-    }
   }
 }
