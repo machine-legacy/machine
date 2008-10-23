7ca7caef0dbd7f48051b4894c396fa8278344adf
Jacob Lewallen <jlewallen@gmail.com>
2008-10-23 13:44:53 -0700

    Cleaning up the dispatch code, grouping some data together.

--- Source/Extensions/Machine.MassTransitExtensions/LowerLevelMessageBus/MessageDispatcher.cs
+++ Source/Extensions/Machine.MassTransitExtensions/LowerLevelMessageBus/MessageDispatcher.cs
@@ -18,14 +18,15 @@ namespace Machine.MassTransitExtensions.LowerLevelMessageBus
 
     public void Dispatch(IMessage message)
     {
+      Type messageType = message.GetType();
       IList<object> handlers = _container.Resolve.All(delegate(Type handlerType) {
-        return IsAcceptableHandler(handlerType, message.GetType());
+        return IsAcceptableHandler(handlerType, messageType);
       });
       foreach (object handler in handlers)
       {
-        foreach (Type interfaceType in EnumerateHandlerImplementationsOf(handler.GetType(), message.GetType()))
+        foreach (HandlerConsumption consumption in EnumerateHandlerImplementationsOf(handler.GetType(), messageType))
         {
-          IInvoker invoker = Invokers.CreateFor(interfaceType.GetGenericArguments()[0]);
+          IInvoker invoker = Invokers.CreateFor(consumption.MessageType);
           invoker.Dispatch(message, handler);
         }
       }
@@ -36,7 +37,7 @@ namespace Machine.MassTransitExtensions.LowerLevelMessageBus
       return typeof(Consumes<>.All).MakeGenericType(messageType);
     }
 
-    private static IEnumerable<Type> EnumerateHandlerImplementationsOf(Type handlerType, Type messageType)
+    private static IEnumerable<HandlerConsumption> EnumerateHandlerImplementationsOf(Type handlerType, Type messageType)
     {
       foreach (Type interfaceType in handlerType.GetInterfaces())
       {
@@ -51,18 +52,47 @@ namespace Machine.MassTransitExtensions.LowerLevelMessageBus
         }
         if (MakeHandlerType(wouldBeMessageType).IsAssignableFrom(interfaceType))
         {
-          yield return interfaceType;
+          yield return new HandlerConsumption(handlerType, wouldBeMessageType, interfaceType);
         }
       }
     }
 
     private static bool IsAcceptableHandler(Type handlerType, Type messageType)
     {
-      foreach (Type interfaceType in EnumerateHandlerImplementationsOf(handlerType, messageType))
+      foreach (HandlerConsumption consumption in EnumerateHandlerImplementationsOf(handlerType, messageType))
       {
         return true;
       }
       return false;
+    }
+  }
+
+  public class HandlerConsumption
+  {
+    readonly Type _handlerType;
+    readonly Type _messageType;
+    readonly Type _interfaceType;
+
+    public Type HandlerType
+    {
+      get { return _handlerType; }
+    }
+
+    public Type MessageType
+    {
+      get { return _messageType; }
+    }
+
+    public Type InterfaceType
+    {
+      get { return _interfaceType; }
+    }
+
+    public HandlerConsumption(Type handlerType, Type messageType, Type interfaceType)
+    {
+      _handlerType = handlerType;
+      _messageType = messageType;
+      _interfaceType = interfaceType;
     }
   }
 }
