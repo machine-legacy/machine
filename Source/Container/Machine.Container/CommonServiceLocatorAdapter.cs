24f6da854113f4c4629d33c2383d0d722e605874
Aaron Jensen <aaronjensen@gmail.com>
2008-12-21 14:05:27 -0800

    Implementing CSL adapter

---
+++ Source/Container/Machine.Container/CommonServiceLocatorAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Machine.Container.Services;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Machine.Container
+{
+  public class CommonServiceLocatorAdapter : ServiceLocatorImplBase
+  {
+    readonly IHighLevelContainer _container;
+
+    public CommonServiceLocatorAdapter(IHighLevelContainer container)
+    {
+      _container = container;
+    }
+
+    protected override object DoGetInstance(Type serviceType, string key)
+    {
+      if (!string.IsNullOrEmpty(key))
+      {
+        throw new NotImplementedException();
+      }
+
+      return _container.Resolve.Object(serviceType);
+    }
+
+    protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+    {
+      return _container.Resolve.All(serviceType);
+    }
+  }
+}
