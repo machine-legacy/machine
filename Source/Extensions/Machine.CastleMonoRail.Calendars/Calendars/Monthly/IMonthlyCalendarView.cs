06799cd22b9e12b5cac39cd511a9b155acfaf60e
Daniel Tabuenca <dan@eleutian.com>
2009-01-28 07:37:17 +0800

    Added getter to IMonthlyCalendarView
    This fixes a bug when trying to generate the adapters in monorail.
    
    Signed-off-by: aaronjensen <aaronjensen@gmail.com>

--- Source/Extensions/Machine.CastleMonoRail.Calendars/Calendars/Monthly/IMonthlyCalendarView.cs
+++ Source/Extensions/Machine.CastleMonoRail.Calendars/Calendars/Monthly/IMonthlyCalendarView.cs
@@ -6,15 +6,18 @@ namespace Machine.CastleMonoRail.Calendars.Monthly
   public interface IMonthlyCalendarView : IMonthlyCalendarParameters
   {
     MonthlyNavigationParameters Navigation
-    {
+    { 
+      get;
       set;
     }
     MonthlyColumnParameters Column
     {
+      get;
       set;
     }
     CalendarCellParameters Cell
     {
+      get;
       set;
     }
   }
