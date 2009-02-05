a08ca87c9846edc56852cf0d2218dd008b23af81
Aaron Jensen <aaronjensen@gmail.com>
2009-02-04 19:43:53 -0800

    Adding common extenions methods and updating machine.core to 3.5

---
+++ Source/Machine.Core/Machine.Core/CommonExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Machine.Core
+{
+  public static class CommonExtensionMethods
+  {
+    public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action)
+    {
+      foreach (var item in enumerable)
+      {
+        action(item);
+      }
+    }
+
+    public static bool ElementsEqualInOrder<T>(this IEnumerable<T> left, IEnumerable<T> right)
+    {
+      var rightEnumerator = right.GetEnumerator();
+
+      foreach (var element in left)
+      {
+        if (!rightEnumerator.MoveNext()) return false;
+        
+        if (!element.Equals(rightEnumerator.Current)) return false;
+      }
+
+      if (rightEnumerator.MoveNext()) return false;
+
+      return true;
+    }
+  }
+}
